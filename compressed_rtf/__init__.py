25db0e123d1004f29438f18d9bda30e8b7569021
delimitry <dvalimov@gmail.com>
2018-04-30 02:01:58 +0300

    Update import

--- compressed_rtf/__init__.py
+++ compressed_rtf/__init__.py
@@ -8,6 +8,8 @@ https://msdn.microsoft.com/en-us/library/cc463890(v=exchg.80).aspx
 """
 
 __title__ = 'compressed_rtf'
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 __author__ = 'Dmitry Alimov'
 __license__ = 'MIT'
+
+from .compressed_rtf import compress, decompress
--- compressed_rtf/compressed_rtf.py
+++ compressed_rtf/compressed_rtf.py
@@ -12,7 +12,7 @@ import sys
 from io import BytesIO
 from .crc32 import crc32
 
-__all__ = ['decompress', 'compress']
+__all__ = ['compress', 'decompress']
 
 PY3 = sys.version_info[0] == 3
 
@@ -30,80 +30,10 @@ COMPRESSED = b'LZFu'
 UNCOMPRESSED = b'MELA'
 
 
-def decompress(data):
-    """
-    Decompress data
-    """
-    # set init dict
-    init_dict = list(INIT_DICT)
-    init_dict += b' ' * (MAX_DICT_SIZE - INIT_DICT_SIZE)
-    if len(data) < 16:
-        raise Exception('Data must be at least 16 bytes long')
-    write_offset = INIT_DICT_SIZE
-    output_buffer = b''
-    # make stream
-    in_stream = BytesIO(data)
-
-    # read compressed RTF header
-    comp_size = struct.unpack('<I', in_stream.read(4))[0]
-    raw_size = struct.unpack('<I', in_stream.read(4))[0]
-    comp_type = in_stream.read(4)
-    crc_value = struct.unpack('<I', in_stream.read(4))[0]
-
-    # get only data
-    contents = BytesIO(in_stream.read(comp_size - 12))
-
-    if comp_type == COMPRESSED:
-        # check CRC
-        if crc_value != crc32(contents.read()):
-            raise Exception('CRC is invalid! The file is corrupt!')
-        contents.seek(0)
-        end = False
-        while not end:
-            val = contents.read(1)
-            if not val:
-                break
-            control = '{0:08b}'.format(ord(val))
-            # check bits from LSB to MSB
-            for i in range(1, 9):
-                if control[-i] == '1':
-                    # token is reference (16 bit)
-                    val = contents.read(2)
-                    if not val:
-                        break
-                    token = struct.unpack('>H', val)[0]  # big-endian
-                    # extract [12 bit offset][4 bit length]
-                    offset = (token >> 4) & 0b111111111111
-                    length = token & 0b1111
-                    # end indicator
-                    if write_offset == offset:
-                        end = True
-                        break
-                    actual_length = length + 2
-                    for step in range(actual_length):
-                        read_offset = (offset + step) % MAX_DICT_SIZE
-                        char = init_dict[read_offset]
-                        output_buffer += bytes([char]) if PY3 else char
-                        init_dict[write_offset] = char
-                        write_offset = (write_offset + 1) % MAX_DICT_SIZE
-                else:
-                    # token is literal (8 bit)
-                    val = contents.read(1)
-                    if not val:
-                        break
-                    output_buffer += val
-                    init_dict[write_offset] = ord(val) if PY3 else val
-                    write_offset = (write_offset + 1) % MAX_DICT_SIZE
-    elif comp_type == UNCOMPRESSED:
-        return contents.read(raw_size)
-    else:
-        raise Exception('Unknown type of RTF compression!')
-    return output_buffer
-
-
 def compress(data, compressed=True):
     """
-    Compress `data` with `compressed` flag
+    Compress `data` using RTF compression algorithm
+    If `compressed` flag is False, data will be written uncompressed
     """
     output_buffer = b''
     # set init dict
@@ -179,6 +109,77 @@ def compress(data, compressed=True):
     raw_size = struct.pack('<I', len(data))
     crc_value = struct.pack('<I', crc32(output_buffer))
     return comp_size + raw_size + comp_type + crc_value + output_buffer
+
+
+def decompress(data):
+    """
+    Decompress `data` using RTF compression algorithm
+    """
+    # set init dict
+    init_dict = list(INIT_DICT)
+    init_dict += b' ' * (MAX_DICT_SIZE - INIT_DICT_SIZE)
+    if len(data) < 16:
+        raise Exception('Data must be at least 16 bytes long')
+    write_offset = INIT_DICT_SIZE
+    output_buffer = b''
+    # make stream
+    in_stream = BytesIO(data)
+
+    # read compressed RTF header
+    comp_size = struct.unpack('<I', in_stream.read(4))[0]
+    raw_size = struct.unpack('<I', in_stream.read(4))[0]
+    comp_type = in_stream.read(4)
+    crc_value = struct.unpack('<I', in_stream.read(4))[0]
+
+    # get only data
+    contents = BytesIO(in_stream.read(comp_size - 12))
+
+    if comp_type == COMPRESSED:
+        # check CRC
+        if crc_value != crc32(contents.read()):
+            raise Exception('CRC is invalid! The file is corrupt!')
+        contents.seek(0)
+        end = False
+        while not end:
+            val = contents.read(1)
+            if not val:
+                break
+            control = '{0:08b}'.format(ord(val))
+            # check bits from LSB to MSB
+            for i in range(1, 9):
+                if control[-i] == '1':
+                    # token is reference (16 bit)
+                    val = contents.read(2)
+                    if not val:
+                        break
+                    token = struct.unpack('>H', val)[0]  # big-endian
+                    # extract [12 bit offset][4 bit length]
+                    offset = (token >> 4) & 0b111111111111
+                    length = token & 0b1111
+                    # end indicator
+                    if write_offset == offset:
+                        end = True
+                        break
+                    actual_length = length + 2
+                    for step in range(actual_length):
+                        read_offset = (offset + step) % MAX_DICT_SIZE
+                        char = init_dict[read_offset]
+                        output_buffer += bytes([char]) if PY3 else char
+                        init_dict[write_offset] = char
+                        write_offset = (write_offset + 1) % MAX_DICT_SIZE
+                else:
+                    # token is literal (8 bit)
+                    val = contents.read(1)
+                    if not val:
+                        break
+                    output_buffer += val
+                    init_dict[write_offset] = ord(val) if PY3 else val
+                    write_offset = (write_offset + 1) % MAX_DICT_SIZE
+    elif comp_type == UNCOMPRESSED:
+        return contents.read(raw_size)
+    else:
+        raise Exception('Unknown type of RTF compression!')
+    return output_buffer
 
 
 def _find_longest_match(init_dict, stream, write_offset):
