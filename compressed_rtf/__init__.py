ad93a504ca6a09aff9561eac586ff6d91861d147
Dmitry Alimov <dvalimov@gmail.com>
2020-02-16 22:54:26 +0300

    Add new Python versions to CI, update version

--- compressed_rtf/__init__.py
+++ compressed_rtf/__init__.py
@@ -8,7 +8,7 @@ https://msdn.microsoft.com/en-us/library/cc463890(v=exchg.80).aspx
 """
 
 __title__ = 'compressed_rtf'
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 __author__ = 'Dmitry Alimov'
 __license__ = 'MIT'
 
