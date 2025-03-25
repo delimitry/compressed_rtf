478b00e7b7b357eebdbeeb173677363cbae1b50f
Dmitry Alimov <dvalimov@gmail.com>
2025-03-25 17:42:04 +0100

    Update __version__.py

--- compressed_rtf/__version__.py
+++ compressed_rtf/__version__.py
@@ -1,5 +1,5 @@
 __title__ = 'compressed_rtf'
-__version__ = '1.0.7'
+__version__ = '1.0.8'
 __author__ = 'Dmitry Alimov'
 __license__ = 'MIT'
 __description__ = 'Compressed Rich Text Format (RTF) compression and decompression package'
